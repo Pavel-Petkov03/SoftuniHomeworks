df76e2cbda72aeacebad1f2875fc41158122be2a
Pavel <elias5247698@abv.bg>
2021-03-30 01:46:38 +0300

    try for the laste exercise

--- Programming_OOP_with_Python/classes_and_instances_exercise/library/project/library.py
+++ Programming_OOP_with_Python/classes_and_instances_exercise/library/project/library.py
@@ -1,4 +1,4 @@
-from user import User
+
 
 
 class Library:
@@ -6,13 +6,13 @@ class Library:
     books_available = {}
     rented_books = {}
 
-    def add_user(self, user: User):
+    def add_user(self, user):
         if user not in Library.user_records:
             Library.user_records.append(user)
         else:
-            return f"User with id = {user.id} already registered in the library!"
+            return f"User with id = {user.user_id} already registered in the library!"
 
-    def remove_user(self, user: User):
+    def remove_user(self, user):
         if user in Library.user_records:
             Library.user_records.remove(user)
         else:
@@ -20,4 +20,13 @@ class Library:
         # rented books later
 
     def change_username(self, user_id: int, new_username: str):
-        pass
+        find = [c for c in Library.user_records if c.user_id == user_id]
+        if find:
+            user_object = find[0]
+            if user_object.username == new_username:
+                return "Please check again the provided username - it should be different than the username used so far!"
+            Library.rented_books[new_username] = 1
+            Library.user_records[Library.user_records.index(user_object)].username = new_username
+            return f"Username successfully changed to: {new_username} for userid: {user_id}"
+
+        return f"There is no user with id = {user_id}!"
--- Programming_OOP_with_Python/classes_and_instances_exercise/library/project/user.py
+++ Programming_OOP_with_Python/classes_and_instances_exercise/library/project/user.py
@@ -1,3 +1,51 @@
+from library import Library
+
+
 class User:
-    def __init__(self,id_):
-        self.id = id_
+    books = []
+
+    def __init__(self, id_, username):
+        self.user_id = id_
+        self.username = username
+
+    def get_book(self, author: str, book_name: str, days_to_return: int, library: Library):
+        if author in library.books_available and book_name in library.books_available[author]:
+            if book_name in Library.rented_books:
+                return f"The book \"{book_name}\" is already rented and will be available in {Library.rented_books[book_name]} days!"
+            library.books_available[author].append(book_name)
+            User.books.append(book_name)
+            library.rented_books.update({self.username:{book_name:days_to_return}})
+
+    def return_book(self, author: str, book_name: str, library: Library):
+        pass
+
+
+user = User(12, 'Peter')
+library = Library()
+library.add_user(user)
+print(library.add_user(user))
+library.remove_user(user)
+print(library.remove_user(user))
+library.add_user(user)
+print(library.change_username(2, 'Igor'))
+print(library.change_username(12, 'Peter'))
+print(library.change_username(12, 'George'))
+
+[print(f'{user_record.user_id}, {user_record.username}, {user_record.books}') for user_record in library.user_records]
+library.books_available.update({'J.K.Rowling': ['The Chamber of Secrets',
+                                                'The Prisoner of Azkaban',
+                                                'The Goblet of Fire',
+                                                'The Order of the Phoenix',
+                                                'The Half-Blood Prince',
+                                                'The Deathly Hallows']})
+
+user.get_book('J.K.Rowling', 'The Deathly Hallows', 17, library)
+print(library.books_available)
+print(library.rented_books)
+print(user.books)
+print(user.get_book('J.K.Rowling', 'The Deathly Hallows', 10, library))
+print(user.return_book('J.K.Rowling', 'The Cursed Child', library))
+user.return_book('J.K.Rowling', 'The Deathly Hallows', library)
+print(library.books_available)
+print(library.rented_books)
+print(user.books)
