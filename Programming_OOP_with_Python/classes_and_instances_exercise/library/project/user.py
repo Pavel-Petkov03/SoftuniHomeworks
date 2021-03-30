c1e8b0895f893dfa05f200698325015faf0ed4d1
Pavel <elias5247698@abv.bg>
2021-03-30 21:58:59 +0300

    new commit

--- Programming_OOP_with_Python/classes_and_instances_exercise/library/project/user.py
+++ Programming_OOP_with_Python/classes_and_instances_exercise/library/project/user.py
@@ -1,51 +1,33 @@
-from library import Library
-
 
 class User:
     books = []
-
-    def __init__(self, id_, username):
+    def __init__(self,id_,username):
         self.user_id = id_
         self.username = username
 
-    def get_book(self, author: str, book_name: str, days_to_return: int, library: Library):
+    def get_book(self, author: str, book_name: str, days_to_return: int, library):
         if author in library.books_available and book_name in library.books_available[author]:
-            if book_name in Library.rented_books:
-                return f"The book \"{book_name}\" is already rented and will be available in {Library.rented_books[book_name]} days!"
-            library.books_available[author].append(book_name)
+            if self.username in library.rented_books and book_name in library.rented_books[self.username]:
+                library.rented_books[self.username][book_name] = days_to_return
+                return f"The book \"{book_name}\" is already rented and will be available in {days_to_return} days!"
+            library.books_available[author].remove(book_name)
             User.books.append(book_name)
-            library.rented_books.update({self.username:{book_name:days_to_return}})
+            library.rented_books[self.username] = {book_name : days_to_return}
+            return f"{book_name} successfully rented for the next {days_to_return} days!"
 
-    def return_book(self, author: str, book_name: str, library: Library):
-        pass
+    def return_book(self, author:str, book_name:str, library):
+        if book_name in User.books:
+            library.rented_books[self.username].pop(book_name)
+            library.books_available[author].remove(book_name)
+
+        return f"{self.username} doesn't have this book in his/her records!"
+
+    def info(self):
+        return ', '.join([])
+
+    def __str__(self):
+        return f"{self.user_id}, {self.username}, {User.books}"
 
 
-user = User(12, 'Peter')
-library = Library()
-library.add_user(user)
-print(library.add_user(user))
-library.remove_user(user)
-print(library.remove_user(user))
-library.add_user(user)
-print(library.change_username(2, 'Igor'))
-print(library.change_username(12, 'Peter'))
-print(library.change_username(12, 'George'))
 
-[print(f'{user_record.user_id}, {user_record.username}, {user_record.books}') for user_record in library.user_records]
-library.books_available.update({'J.K.Rowling': ['The Chamber of Secrets',
-                                                'The Prisoner of Azkaban',
-                                                'The Goblet of Fire',
-                                                'The Order of the Phoenix',
-                                                'The Half-Blood Prince',
-                                                'The Deathly Hallows']})
 
-user.get_book('J.K.Rowling', 'The Deathly Hallows', 17, library)
-print(library.books_available)
-print(library.rented_books)
-print(user.books)
-print(user.get_book('J.K.Rowling', 'The Deathly Hallows', 10, library))
-print(user.return_book('J.K.Rowling', 'The Cursed Child', library))
-user.return_book('J.K.Rowling', 'The Deathly Hallows', library)
-print(library.books_available)
-print(library.rented_books)
-print(user.books)
