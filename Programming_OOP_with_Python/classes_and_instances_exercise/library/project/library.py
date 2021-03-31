d3d5202caf474f466348f9d269ec248b9054b9ad
Pavel <elias5247698@abv.bg>
2021-03-31 13:32:58 +0300

    new commit for classes and instances

--- Programming_OOP_with_Python/classes_and_instances_exercise/library/project/library.py
+++ Programming_OOP_with_Python/classes_and_instances_exercise/library/project/library.py
@@ -1,64 +1,31 @@
-from user import User
-
-
 class Library:
-    user_records = []
-    books_available = {}
-    rented_books = {}
 
-    def add_user(self, user: User):
-        if user in Library.user_records:
+    def __init__(self):
+        self.user_records = []
+        self.books_available = {}
+        self.rented_books = {}
+    def add_user(self, user):
+        if user in self.user_records:
             return f"User with id = {user.user_id} already registered in the library!"
-        Library.user_records.append(user)
+        self.user_records.append(user)
 
-    def remove_user(self, user: User):
-        if user in Library.user_records:
-            Library.user_records.remove(user)
-            if user.username in Library.rented_books:
-                Library.rented_books.pop(user.username)
+    def remove_user(self, user):
+        if user in self.user_records:
+            self.user_records.remove(user)
+            if user.username in self.rented_books:
+                self.rented_books.pop(user.username)
         return "We could not find such user to remove!"
 
     def change_username(self, user_id: int, new_username: str):
-        find = [user for user in Library.user_records if user.user_id == user_id]
+        find = [user for user in self.user_records if user.user_id == user_id]
         if find:
             actual_object = find[0]
-            place = Library.user_records.index(actual_object)
+            place = self.user_records.index(actual_object)
             if actual_object.username == new_username:
                 return "Please check again the provided username - it should be different than the username used so far!"
-            Library.user_records[place].username = new_username
-            if new_username in Library.rented_books:
-                Library.rented_books.pop(new_username)
+            self.user_records[place].username = new_username
+            if new_username in self.rented_books:
+                self.rented_books.pop(new_username)
             return f"Username successfully changed to: {new_username} for userid: {user_id}"
 
         return f"There is no user with id = {user_id}!"
-
-user = User(12, 'Peter')
-library = Library()
-library.add_user(user)
-print(library.add_user(user))
-library.remove_user(user)
-print(library.remove_user(user))
-library.add_user(user)
-print(library.change_username(2, 'Igor'))
-print(library.change_username(12, 'Peter'))
-print(library.change_username(12, 'George'))
-
-[print(f'{user_record.user_id}, {user_record.username}, {user_record.books}') for user_record in library.user_records]
-library.books_available.update({'J.K.Rowling': ['The Chamber of Secrets',
-                                                'The Prisoner of Azkaban',
-                                                'The Goblet of Fire',
-                                                'The Order of the Phoenix',
-                                                'The Half-Blood Prince',
-                                                'The Deathly Hallows']})
-
-
-user.get_book('J.K.Rowling', 'The Deathly Hallows', 17, library)
-print(library.books_available)
-print(library.rented_books)
-print(user.books)
-print(user.get_book('J.K.Rowling', 'The Deathly Hallows', 10, library))
-print(user.return_book('J.K.Rowling', 'The Cursed Child', library))
-user.return_book('J.K.Rowling', 'The Deathly Hallows', library)
-print(library.books_available)
-print(library.rented_books)
-print(user.books)
--- Programming_OOP_with_Python/classes_and_instances_exercise/library/project/user.py
+++ Programming_OOP_with_Python/classes_and_instances_exercise/library/project/user.py
@@ -1,33 +1,34 @@
 
 class User:
     books = []
-    def __init__(self,id_,username):
+
+    def __init__(self, id_, username):
         self.user_id = id_
         self.username = username
 
     def get_book(self, author: str, book_name: str, days_to_return: int, library):
+        if self.username in library.rented_books and book_name in library.rented_books[self.username]:
+            return f"The book \"{book_name}\" is already rented and will be available in {days_to_return} days!"
         if author in library.books_available and book_name in library.books_available[author]:
-            if self.username in library.rented_books and book_name in library.rented_books[self.username]:
-                library.rented_books[self.username][book_name] = days_to_return
-                return f"The book \"{book_name}\" is already rented and will be available in {days_to_return} days!"
             library.books_available[author].remove(book_name)
-            User.books.append(book_name)
-            library.rented_books[self.username] = {book_name : days_to_return}
+            if book_name not in User.books:
+                User.books.append(book_name)
+            library.rented_books[self.username] = {book_name: days_to_return}
             return f"{book_name} successfully rented for the next {days_to_return} days!"
 
-    def return_book(self, author:str, book_name:str, library):
+    def return_book(self, author: str, book_name: str, library):
         if book_name in User.books:
             library.rented_books[self.username].pop(book_name)
-            library.books_available[author].remove(book_name)
+            library.books_available[author].append(book_name)
+            User.books.remove(book_name)
 
         return f"{self.username} doesn't have this book in his/her records!"
 
     def info(self):
-        return ', '.join([])
+        return ', '.join(User.books)
 
     def __str__(self):
         return f"{self.user_id}, {self.username}, {User.books}"
-
 
 
 
