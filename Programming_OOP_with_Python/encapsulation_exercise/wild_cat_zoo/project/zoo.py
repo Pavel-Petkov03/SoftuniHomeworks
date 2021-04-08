ead7d453f03335fcea5e6d8e663e8445805d73a1
Pavel <elias5247698@abv.bg>
2021-04-08 21:38:12 +0300

    new commit

--- Programming_OOP_with_Python/encapsulation_exercise/wild_cat_zoo/project/zoo.py
+++ Programming_OOP_with_Python/encapsulation_exercise/wild_cat_zoo/project/zoo.py
@@ -1,3 +1,4 @@
+
 class Zoo:
     def __init__(self, name, budget, animals_capacity, workers_capacity):
         self.__animals_capacity = animals_capacity
@@ -30,3 +31,30 @@ class Zoo:
             self.workers.remove(worker_obj)
             return f"{worker_name} fired successfully"
         return f"There is no {worker_name} in the zoo"
+
+    def pay_workers(self):
+        if self.__budget >= sum([obj.salary for obj in self.workers]):
+            self.__budget -= sum([obj.salary for obj in self.workers])
+            return f"You payed your workers. They are happy. Budget left: {self.__budget}"
+        return f"You have no budget to pay your workers. They are unhappy"
+
+    def tend_animals(self):
+        if self.__budget >= sum([obj.get_needs() for obj in self.animals]):
+            self.__budget -= sum([obj.get_needs() for obj in self.animals])
+            return f"You tended all the animals. They are happy. Budget left: {self.__budget}"
+        return "You have no budget to tend the animals. They are unhappy."
+
+    def profit(self,amount):
+        self.__budget += amount
+
+    def animals_status(self):
+        result = f'You have {len(self.animals)} animals\n'
+        filters_lions = [lion for lion in self.animals if lion.NAME_OF_KIND == 'Lion']
+        filtered_cheetahs = [cheetah for cheetah in self.animals if cheetah.NAME_OF_KIND == 'Cheetah']
+        filtered_tigers = [tiger for tiger in self.animals if tiger.NAME_OF_KIND == 'Tiger']
+        result += f'----- {len(filters_lions)} Lions:\n'
+        for lion in filters_lions:
+            result += str(lion)
+
+
+
