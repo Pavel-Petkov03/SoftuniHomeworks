c25659fa6f005ee052ab44b86e2533f0d2e334c6
Pavel <elias5247698@abv.bg>
2021-04-11 16:24:12 +0300

    new commit

--- Programming_OOP_with_Python/encapsulation_exercise/pizza_calories/project/pizza.py
+++ Programming_OOP_with_Python/encapsulation_exercise/pizza_calories/project/pizza.py
@@ -1,6 +1,3 @@
-
-
-
 class Pizza:
     def __init__(self, name, dough, toppings_capacity):
         self.__name = name
@@ -32,6 +29,14 @@ class Pizza:
     def toppings_capacity(self, new_value):
         self.__toppings_capacity = new_value
 
+    @property
+    def toppings(self):
+        return self.__toppings
+
+    @toppings.setter
+    def toppings(self, new_value):
+        self.__toppings = new_value
+
     def add_topping(self, topping):
         if self.__toppings_capacity == len(self.__toppings):
             raise ValueError('Not enough space for another topping')
@@ -41,6 +46,4 @@ class Pizza:
             self.__toppings[topping.topping_type] = topping.weight
 
     def calculate_total_weight(self):
-        return sum([weight for type_, weight in self.__toppings.items()])
-
-
+        return sum([weight for type_, weight in self.__toppings.items()]) + self.__dough.weight
