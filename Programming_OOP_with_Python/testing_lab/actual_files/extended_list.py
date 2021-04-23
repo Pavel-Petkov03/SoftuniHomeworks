2b3b1d77d98ad5d579dddcca593014e8e005b8d0
Pavel <elias5247698@abv.bg>
2021-04-23 20:11:02 +0300

    new commit for testing

--- Programming_OOP_with_Python/testing_lab/actual_files/extended_list.py
+++ Programming_OOP_with_Python/testing_lab/actual_files/extended_list.py
@@ -4,99 +4,106 @@ class IntegerList:
         for x in args:
             if type(x) == int:
                 self.__data.append(x)
- 
+
     def get_data(self):
         return self.__data
- 
+
     def add(self, element):
         if not type(element) == int:
             raise ValueError("Element is not Integer")
         self.get_data().append(element)
         return self.get_data()
- 
+
     def remove_index(self, index):
         if index >= len(self.get_data()):
             raise IndexError("Index is out of range")
         a = self.get_data()[index]
         del self.get_data()[index]
         return a
- 
+
     def get(self, index):
         if index >= len(self.get_data()):
             raise IndexError("Index is out of range")
         return self.get_data()[index]
- 
+
     def insert(self, index, el):
         if index >= len(self.get_data()):
             raise IndexError("Index is out of range")
         elif not type(el) == int:
             raise ValueError("Element is not Integer")
- 
+
         self.get_data().insert(index, el)
- 
+
     def get_biggest(self):
         a = sorted(self.get_data(), reverse=True)
         return a[0]
- 
+
     def get_index(self, el):
         return self.get_data().index(el)
+
 
 import unittest
 
 
-class MyTest(unittest.TestCase):
-
+class IntegerListTests(unittest.TestCase):
     def setUp(self):
-        self.list = IntegerList(1, 2, 3, 4, 5)
+        self.my_list = IntegerList(1, 2, 3, 4)
 
-    def test_for_add_func_with_another_type__expected_error(self):
+    def test_add_function_element__with_not_int_element__expected_value_error(self):
         with self.assertRaises(ValueError):
-            self.list.add('12')
+            self.my_list.add('12')
 
-    def test_for_add_function_append_new_element(self):
-        self.list.add(7)
-        self.assertListEqual([1, 2, 3, 4, 5, 7], self.list.get_data())
+    def test_add_function_with_int_element__expected_append_to_list_and_return_the_whole_list(self):
+        self.my_list.add(5)
+        expected = self.my_list.get_data()
+        actual = [1, 2, 3, 4, 5]
+        self.assertListEqual(expected, actual)
 
-    def test_for_remove_index__index_not_in_range__expected_error(self):
+    def test_remove_index_index_greater_than_len_of_list__expected_index_out_of_range_error(self):
         with self.assertRaises(IndexError):
-            self.list.remove_index(6)
+            self.my_list.remove_index(10)
 
-    def test_for_remove_index__expected_remove_the_current_index_and_return_the_result(self):
-        expected = 1
-        actual = self.list.remove_index(0)
+    def test_remove_valid_index_and_return_the_element_from_the_index_and_remove_it(self):
+        actual_return = self.my_list.remove_index(0)
+        expected_return = 1
+        expected_final_list = [2, 3, 4]
+        self.assertEqual(expected_return, actual_return)
+        self.assertListEqual(self.my_list.get_data(), expected_final_list)
+
+    def test_get_function_with_invalid_index__expected_index_error(self):
+        with self.assertRaises(IndexError):
+            self.my_list.get(10)
+
+    def test_get_function_with_valid_index__expected_element_from_index_in_the_list(self):
+        expected = 0
+        actual = self.my_list.get_index(1)
         self.assertEqual(expected, actual)
 
-    def test_get_element_by_index_from_the_list__expected_return_element(self):
-        expected = 1
-        actual = self.list.get(0)
-        self.assertEqual(expected, actual)
-
-    def test_get_index_in_list__index_not_in_range__expected_error(self):
+    def test_insert_function_with_invalid_index__expected_index_out_of_range_error(self):
         with self.assertRaises(IndexError):
-            self.list.get(6)
+            self.my_list.insert(10, 5)
 
-    def test_insert_method_index_not_in_range__expected_index_error(self):
-        with self.assertRaises(IndexError):
-            self.list.insert(10, 2)
-
-    def test_insert_method_el_not_int__expected__value_error(self):
+    def test_insert_function_with_invalid_type__expected_value_error(self):
         with self.assertRaises(ValueError):
-            self.list.insert(0, True)
+            self.my_list.insert(0, '12')
 
-    def test_insert_method_everything_is_fine_and_insert_element_to_the_given_index(self):
-        self.list.insert(0, 0)
-        expected = [0, 1, 2, 3, 4, 5]
-        self.assertListEqual(expected, self.list.get_data())
+    def test_insert_function_everything_is_valid__insert_el_to_the_given_index(self):
+        expected_final_list = [1, 2, 3, 12, 4]
+        self.my_list.insert(3, 12)
 
-    def test_get_biggest_expect_get_the_biggest_element_from_the_list(self):
-        expected_max_el = 5
-        self.assertEqual(expected_max_el, self.list.get_biggest())
+        self.assertListEqual(expected_final_list, self.my_list.get_data())
 
-    def test_get_index_if_el_in_list_and_return_element(self):
-        actual_index = self.list.get_index(1)
-        expected_index = 0
-        self.assertEqual(expected_index, actual_index)
+    def test_get_biggest_function_expected_biggest_element(self):
+        expected_max_element = 4
+        actual_max = self.my_list.get_biggest()
+        self.assertEqual(expected_max_element, actual_max)
+
+    def test_get_index_function_expected_return_element_to_the_given_index(self):
+        expected = 3
+        actual = self.my_list.get_index(4)
+        self.assertEqual(expected, actual)
 
 
 if __name__ == "__main___":
     unittest.main()
+
