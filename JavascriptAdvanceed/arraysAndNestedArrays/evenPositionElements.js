e5f2293646228cba9f4817a724ede9719de15c38
Pavel <elias5247698@abv.bg>
2021-06-25 01:29:13 +0300

    new commit for js

--- JavascriptAdvanceed/arraysAndNestedArrays/evenPositionElements.js
+++ JavascriptAdvanceed/arraysAndNestedArrays/evenPositionElements.js
@@ -91,23 +91,25 @@ function diagonalSum(matrix){
 }
 
 
-function findEqualNeighbors(matrix){
-    function findNeighbors(matrix, row , col){
-    let find = 0
-    let symbol = matrix[row][col]
-    if ( row+1 >= 0 && row +1 < matrix.length && matrix[row+1][col] === symbol){
-        find++
+function findEqualNeighbors(matrix) {
+    function findNeighbors(matrix, row, col) {
+        let find = 0
+        let symbol = matrix[row][col]
+        if (row + 1 >= 0 && row + 1 < matrix.length && matrix[row + 1][col] === symbol) {
+            find++
+        }
+        if (col + 1 >= 0 && col + 1 < matrix[0].length && matrix[row][col + 1] === symbol) {
+            find++
+        }
+        return find
     }
-    if(col +1 >= 0 && col +1 <  matrix[0].length && matrix[row][col+1] === symbol){
-        find++
-    }
-    return find
-    }
+
     let all = 0
-    for (let row = 0; row < matrix.length; row++){
-        for( let col = 0; col < matrix[row].length; col++){
-            all += findNeighbors(matrix , row , col)
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[row].length; col++) {
+            all += findNeighbors(matrix, row, col)
         }
     }
     console.log(all)
 }
+
