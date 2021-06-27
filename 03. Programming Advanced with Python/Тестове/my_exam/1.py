952e9aa0860a9c44aeb41cf197ba66fd99218410
Pavel <elias5247698@abv.bg>
2021-06-27 21:59:41 +0300

    nre commit for taken exam

--- 03. Programming Advanced with Python/Тестове/my_exam/1.py
+++ 03. Programming Advanced with Python/Тестове/my_exam/1.py
@@ -1,58 +1,35 @@
 from collections import deque
 
+milkshake = 0
+choco = list(map(int, input().split(', ')))
+milks = deque([int(c) for c in input().split(', ')])
+while choco and milks:
+    if choco[-1] <= 0 or milks[0] <= 0:
+        if choco[-1] <= 0:
+            choco.pop()
+        if milks[0] <= 0:
+            milks.popleft()
+        continue
 
-class First:
-    def __init__(self):
-        self.males = list(map(int, input().split()))
-        self.females = deque(map(int, input().split()))
-        self.matches = 0
+    if choco[-1] == milks[0]:
+        choco.pop()
+        milks.popleft()
+        milkshake += 1
+    else:
+        milks.append(milks.popleft())
+        choco[-1] -= 5
+    if milkshake == 5:
+        break
 
-    def main(self):
-        while self.males and self.females:
-            if self.males[-1] <= 0:
-                self.pop_male()
-                continue
-            if self.females[0] <= 0:
-                self.pop_female()
-                continue
-            if self.remove_next_from_same_gender((self.males[-1], self.females[-1])) is None:
-                continue
-            if self.males[-1] == self.females[0]:
-                self.matches += 1
-                self.pop_male()
-                self.pop_female()
-            else:
-                self.pop_female()
-                self.males[-1] -= 2
-
-    def pop_male(self):
-        self.males.pop()
-
-    def pop_female(self):
-        self.females.popleft()
-
-    def remove_next_from_same_gender(self, person):
-        if person[0] % 25 == 0:
-            self.pop_male()
-            if self.males:
-                self.pop_male()
-                return
-        if person[1] % 25 == 0:
-            self.pop_female()
-            if self.females:
-                self.pop_female()
-                return
-        return True
-
-    def print_result(self):
-        result = [
-            f"Matches: {self.matches}",
-            f"Males left: {', '.join([str(male) for male in reversed(self.males)])}" if self.males else "Males left: none",
-            f"Females left: {', '.join([str(female) for female in self.females])}" if self.females else "Females left: none",
-        ]
-        return '\n'.join(result)
-
-
-f = First()
-f.main()
-print(f.print_result())
+if milkshake == 5:
+    print(f'Great! You made all the chocolate milkshakes needed!')
+else:
+    print("Not enough milkshakes.")
+if choco:
+    print(f"Chocolate: {', '.join([str(c) for c in choco])}")
+else:
+    print("Chocolate: empty")
+if milks:
+    print(f"Milk: {', '.join([str(c) for c in milks])}")
+else:
+    print("Milk: empty")
