ea38ff21b974670d65375beb13cba0172e8ed1c9
Pavel <elias5247698@abv.bg>
2021-07-06 22:53:11 +0300

    make new commit on js tictaktoe

--- JavascriptAdvanceed/arraysAndNestedArraysExercise/printAnArrayWithGivenDelimeter.js
+++ JavascriptAdvanceed/arraysAndNestedArraysExercise/printAnArrayWithGivenDelimeter.js
@@ -46,19 +46,17 @@ function createDecreasingSubsequence(array) {
 }
 
 function ListOfNames(array) {
-    return array.sort((a,b) => a.localeCompare(b)).map((el , index) => {
-        return `${index+1}.${el}`
+    return array.sort((a, b) => a.localeCompare(b)).map((el, index) => {
+        return `${index + 1}.${el}`
     }).join('\n')
 }
-
-
 
 
 function sortingNumbers(array) {
     let newArray = []
     let len = array.length
-    array.sort((a,b) => a-b)
-    for(let it=0 ; it < len /2; it++){
+    array.sort((a, b) => a - b)
+    for (let it = 0; it < len / 2; it++) {
         newArray.push(array.shift())
         newArray.push(array.pop())
     }
@@ -68,7 +66,7 @@ function sortingNumbers(array) {
 
 function sortByTwoCriteria(array) {
     array.sort(function (a, b) {
-        return  a- b
+        return a - b
     })
 }
 
@@ -90,24 +88,81 @@ function magicMatrix(matrix) {
 }
 
 
-
 function ticTacToe(matrix) {
     let initialMatrix = [[false, false, false],
         [false, false, false],
         [false, false, false]]
+    let isWinner = false
+    let takenPlaces = 0
+    let startTupleOfCord = 0
     let row
     let col
-    let index = 0
-    for (const cord of matrix) {
-        row = cord[0]
-        col = cord[1]
-        if (matrix[row][col] !== false) {
-            console.log()
-        } else {
-            let checkForWin = (row, col) => {
-
-            }
+    let startPlayer = 'O'
+    let changePlayer = (player) => {
+        if (player === 'X') {
+            return 'O'
+        }
+        return 'X'
+    }
+    while (!isWinner && takenPlaces !== 9) {
+        [row, col] = [...matrix[startTupleOfCord].split(' ').map(x => Number(x))]
+        startTupleOfCord++
+        if (initialMatrix[row][col] === false) {
+            startPlayer = changePlayer(startPlayer)
+            initialMatrix[row][col] = startPlayer
+            takenPlaces++
+            isWinner = checkForWin(initialMatrix)
+        }else{
+            console.log(`This place is already taken. Please choose another!`)
         }
     }
+
+    let printResult  = () => {
+        console.log(`${initialMatrix[0][0]}\t${initialMatrix[0][1]}\t${initialMatrix[0][2]}`)
+        console.log(`${initialMatrix[1][0]}\t${initialMatrix[1][1]}\t${initialMatrix[1][2]}`)
+        console.log(`${initialMatrix[2][0]}\t${initialMatrix[2][1]}\t${initialMatrix[2][2]}`)
+    }
+    if(isWinner){
+        console.log(`Player ${startPlayer} wins!`)
+    }else{
+        console.log(`The game ended! Nobody wins :(`)
+    }
+    printResult()
 }
+
+
+function checkForWin(matrix) {
+    if (matrix[0][0] === matrix[0][1] === matrix[0][2] !== false) {
+        return true
+    } else if (matrix[1][0] === matrix[1][1] === matrix[1][2] !== false) {
+        return true
+    } else if (matrix[2][0] === matrix[2][1] === matrix[2][2] !== false) {
+        return true
+    } else if (matrix[0][0] === matrix[1][1] === matrix[2][2] !== false) {
+        return true
+    } else if (matrix[2][0] === matrix[1][1] === matrix[0][2] !== false) {
+        return true
+    } else if (matrix[0][0] === matrix[0][1] === matrix[0][2] !== false) {
+        return true
+    } else if (matrix[1][0] === matrix[1][1] === matrix[1][2] !== false) {
+        return true
+    } else if (matrix[2][0] === matrix[2][1] === matrix[2][2] !== false) {
+        return true
+    }
+    return false
+}
+
+
+ticTacToe(
+        ["0 1",
+             "0 0",
+             "0 2",
+             "2 0",
+             "1 0",
+             "1 1",
+             "1 2",
+             "2 2",
+             "2 1",
+             "0 0"]
+)
 
