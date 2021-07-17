09a91d8fcc71d6bb2bd1fc0e4e971a51129afa40
Pavel <elias5247698@abv.bg>
2021-07-18 01:54:04 +0300

    new commit for DOM manipulation

--- JavascriptAdvanceed/DOMManipulationAndEventsLab/07. Shopping-Cart/solution.js
+++ JavascriptAdvanceed/DOMManipulationAndEventsLab/07. Shopping-Cart/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-   //TODO...
+    let massiveOfProducts = []
+    let allPrice = 0
+    let arrayOfButtons = Array.from(document.querySelectorAll('.add-product'))
+    let textArea = document.querySelector('textarea')
+    arrayOfButtons.forEach(btn => {
+        btn.addEventListener('click', (ev) => {
+            const parent = ev.target.parentNode.parentNode
+            const product = parent.querySelector('.product-title').textContent
+            const amount = Number(parent.querySelector('.product-line-price').textContent)
+            if(!massiveOfProducts.includes(product)){
+                massiveOfProducts.push(product)
+            }
+            allPrice += amount
+            textArea.value += `Added ${product} for ${amount.toFixed(2)} to the cart.\n`
+        })
+    })
+    document.querySelector('.checkout').addEventListener('click', (ev) => {
+        textArea.value += `You bought ${massiveOfProducts.join(', ')} for ${allPrice.toFixed(2)}.\n`
+        Array.from(document.querySelectorAll('button')).forEach(btn => btn.disabled= true)
+    })
 }
