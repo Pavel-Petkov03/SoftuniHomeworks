97f820f8b5acdd4e84279e5d8b5ddda9af0d3255
Pavel <elias5247698@abv.bg>
2021-07-20 15:10:27 +0300

    new commit for js event listeners"

--- JavascriptAdvanceed/DOMManipulationAndEventsExercise/09. JavaScript-Quiz/solution.js
+++ JavascriptAdvanceed/DOMManipulationAndEventsExercise/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,23 @@
 function solve() {
-  //TODO...
+  const validData = ['onclick', 'JSON.stringify()', 'A programming API for HTML and XML documents'];
+  let arrayOfSections = Array.from(document.querySelectorAll('section'))
+
+    let counter = 0
+    Array.from(document.querySelectorAll('.answer-text')).forEach(btn => {
+        btn.addEventListener('click', (ev) => {
+            let section = ev.target.parentNode.parentNode.parentNode.parentNode
+            if(validData.includes(ev.target.textContent)){
+                counter++
+            }
+            section.style.display = 'none'
+            try {
+                let nextSection = arrayOfSections[arrayOfSections.indexOf(section) + 1]
+                nextSection.style.display = 'block'
+            }catch (TypeError){
+                let result = document.getElementsByTagName('h1')[1]
+                result.parentNode.parentNode.style.display = 'block'
+                result.textContent = counter === 3 ? "You are recognized as top JavaScript fan!" : `You have ${counter} right answers`
+            }
+        })
+    })
 }
