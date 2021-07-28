e0e2062aef2632282b21348e746e16d293b4a5b8
Pavel <elias5247698@abv.bg>
2021-07-28 03:05:46 +0300

    new commit for js

--- JavascriptAdvanceed/DOMManipulationAndEventsExercise/02. Time-Converter/solution.js
+++ JavascriptAdvanceed/DOMManipulationAndEventsExercise/02. Time-Converter/solution.js
@@ -1,4 +1,42 @@
 function attachEventsListeners() {
-
-    console.log('TODO:...');
+    Array.from(document.querySelectorAll('div input:last-child')).forEach(btn => {
+        btn.addEventListener('click' , (ev)=> {
+            let finalHours = 0
+            let finalMinutes = 0
+            let finalDays = 0
+            let finalSeconds = 0
+            const placeText = ev.target.id
+            const value = Number(ev.target.parentNode.querySelector('input').value)
+            switch (placeText){
+                case 'hoursBtn' :
+                    finalDays = value / 24
+                    finalHours = value
+                    finalMinutes = finalHours * 60
+                    finalSeconds = finalMinutes * 60
+                    break
+                case 'daysBtn':
+                    finalDays = value
+                    finalHours = value * 24
+                    finalMinutes = finalHours * 60
+                    finalSeconds = finalMinutes * 60
+                    break
+                case 'minutesBtn':
+                    finalDays = value / 3600
+                    finalHours = value / 60
+                    finalMinutes = value
+                    finalSeconds = finalMinutes * 60
+                    break
+                case 'secondsBtn' :
+                    finalDays = value / 86400
+                    finalHours = value / 3600
+                    finalMinutes = value / 60
+                    finalSeconds = value
+                    break
+            }
+            document.getElementById('days').value = finalDays
+            document.getElementById('hours').value = finalHours
+            document.getElementById('minutes').value = finalMinutes
+            document.getElementById('seconds').value = finalSeconds
+        })
+    })
 }
