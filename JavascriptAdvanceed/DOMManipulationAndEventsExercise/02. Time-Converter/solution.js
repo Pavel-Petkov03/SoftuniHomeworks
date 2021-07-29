5419ab28a5258ef7ffa8377fa8fe7ce283ab5597
Pavel <elias5247698@abv.bg>
2021-07-29 14:04:16 +0300

    new commit for js  advanced functions

--- JavascriptAdvanceed/DOMManipulationAndEventsExercise/02. Time-Converter/solution.js
+++ JavascriptAdvanceed/DOMManipulationAndEventsExercise/02. Time-Converter/solution.js
@@ -1,42 +1,26 @@
 function attachEventsListeners() {
     Array.from(document.querySelectorAll('div input:last-child')).forEach(btn => {
-        btn.addEventListener('click' , (ev)=> {
-            let finalHours = 0
-            let finalMinutes = 0
-            let finalDays = 0
-            let finalSeconds = 0
+        btn.addEventListener('click', (ev) => {
             const placeText = ev.target.id
             const value = Number(ev.target.parentNode.querySelector('input').value)
-            switch (placeText){
-                case 'hoursBtn' :
-                    finalDays = value / 24
-                    finalHours = value
-                    finalMinutes = finalHours * 60
-                    finalSeconds = finalMinutes * 60
-                    break
-                case 'daysBtn':
-                    finalDays = value
-                    finalHours = value * 24
-                    finalMinutes = finalHours * 60
-                    finalSeconds = finalMinutes * 60
-                    break
-                case 'minutesBtn':
-                    finalDays = value / 3600
-                    finalHours = value / 60
-                    finalMinutes = value
-                    finalSeconds = finalMinutes * 60
-                    break
-                case 'secondsBtn' :
-                    finalDays = value / 86400
-                    finalHours = value / 3600
-                    finalMinutes = value / 60
-                    finalSeconds = value
-                    break
+            function convertToDays(measurement, value) {
+                switch (measurement) {
+                    case 'hoursBtn' :
+                        return value / 24
+                    case 'daysBtn' :
+                        return value
+                    case 'minutesBtn' :
+                        return value / 1440
+                    case 'secondsBtn' :
+                        return value / 86400
+                }
             }
-            document.getElementById('days').value = finalDays
-            document.getElementById('hours').value = finalHours
-            document.getElementById('minutes').value = finalMinutes
-            document.getElementById('seconds').value = finalSeconds
+
+            let convertedToDays = convertToDays(placeText, value)
+            document.getElementById('days').value = convertedToDays
+            document.getElementById('hours').value = convertedToDays * 24
+            document.getElementById('minutes').value = convertedToDays * 1440
+            document.getElementById('seconds').value = convertedToDays * 86400
         })
     })
 }
