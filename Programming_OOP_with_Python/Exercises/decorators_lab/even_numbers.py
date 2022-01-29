404ff7a2bce86c14f2d12bea520cfa49014655de
Pavel Petkov <elias@5247698@abv.bg>
2022-01-29 15:45:38 +0200

    change

--- Programming_OOP_with_Python/Exercises/decorators_lab/even_numbers.py
+++ Programming_OOP_with_Python/Exercises/decorators_lab/even_numbers.py
@@ -1,13 +1,37 @@
-def even_numbers(func):
-    def wrapper(*args, **kwargs):
-        return [x for x in func(*args, **kwargs) if x % 2 == 0]
+# def even_numbers(func):
+#     def wrapper(*args, **kwargs):
+#         return [x for x in func(*args, **kwargs) if x % 2 == 0]
+#
+#     return wrapper
+#
+#
+# @even_numbers
+# def get_numbers(numbers):
+#     return numbers
+#
+#
+# print(get_numbers([1, 2, 3, 4, 5]))
 
-    return wrapper
+
+def timer(function):
+    def inner(*args, **kwargs):
+        begin = time.time()
+        function(*args, **kwargs)
+        end = time.time()
+        return f"The total amount of time is {end - begin}"
+
+    return inner
 
 
-@even_numbers
-def get_numbers(numbers):
-    return numbers
+class Calculator:
+    def __init__(self, first_number, second_number):
+        self.first_number = first_number
+        self.second_number = second_number
 
+    def add(self):
+        return self.first_number + self.second_number
 
-print(get_numbers([1, 2, 3, 4, 5]))
+    @timer
+    def find_add_time(self):
+        return self.first_number + self.second_number
+
